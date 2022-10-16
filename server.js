365183f756bff23affd9ae7471def679ffaa1736
Jorge Mena <isc.jorge.mena@gmail.com>
2022-10-16 15:15:46 -0500

    modificar puerto

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3001, () => {
+server.listen(process.env.PORT || 3001, () => {
     console.log('JSON Server is running')
 })
